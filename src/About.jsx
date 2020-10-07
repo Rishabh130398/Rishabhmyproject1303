e763b1d4f98e6313c8793fc2e4d3d8aca181331d
Rishabh130398 <rishabhjain1303@gmail.com>
2020-10-07 15:13:47 +0530

    First Commit

---
+++ src/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+const About=()=> {
+  return (
+    <div >
+     About
+      
+    </div>
+  )
+}
+
+export default About;
---
+++ src/Contact.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+const Contact=()=> {
+  return (
+    <div >
+     Conatact
+      
+    </div>
+  )
+}
+
+export default Contact;
